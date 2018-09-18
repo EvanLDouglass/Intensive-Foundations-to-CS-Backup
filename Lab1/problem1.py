32e7a241617eccf789a96ed4f01f680dc0913715
Evan <evandoug90@gmail.com>
2018-09-17 21:47:39 -0700

    Completed Proplems 1 and 2

---
+++ Lab1/problem1.py
@@ -0,0 +1,34 @@
+'''
+Lab 1
+Problem 1: Eating out with a group
+Evan Douglass
+'''
+
+### Purpose
+# To calculate how to split a restaurant bill between a given number of people.
+# Parameters: total amount of bill, tip percentage as decimal, how many people
+# are splitting the bill.
+# Returns: the amount that each person owes on the bill, rounded to two decimal
+# places.
+#
+### Signature
+# amt_owed :: number, number, number => number
+#
+### Template
+# amt_owed(given...)
+#   return returns...
+#
+### Examples
+# amt_owed(60, 0.2, 3) => 24.0
+# amt_owed(110.90, 0.15, 6) => 21.26
+# amt_owed(20, 1, 1) => 40.0
+
+def amt_owed(total_bill, tip_percent, num_people):
+    tip_amount = total_bill * tip_percent
+    return round((total_bill + tip_amount) / num_people, 2)
+
+# Test runs
+print("Test runs\nExpected results: 24.0, 21.26, 40.0")
+print(amt_owed(60, 0.2, 3))
+print(amt_owed(110.9, 0.15, 6))
+print(amt_owed(20, 1, 1))
---
+++ Lab1/problem2.py
@@ -0,0 +1,43 @@
+'''
+Lab 1
+Problem 2: Buying a house
+Evan Douglass
+'''
+
+### Purpose
+# To find out how long it will take to buy a house given parameters.
+# Parameters: cost of the house, annual salary, percent of salary saved per 
+# month
+# Returns: amount needed for down payment, amount being saved per month, how 
+# many years and months until enough is saved for a down payment...all as a 
+# string.
+#
+### Signature
+# time_to_buy_house :: number, number, number => string
+#
+### Template
+# time_to_buy_house(given...)
+#   return returns...
+#
+### Examples
+# time_to_buy_house(100000, 50000, 0.25) => "If you save $1041.67 per month, 
+# it will take 2 year(s) and 0 month(s) to save enough for the down payment!"
+
+def time_to_buy_house(cost_of_house, annual_salary, percent_salary_saved):
+    # find down payment (25% of cost)
+    down_payment = cost_of_house * 0.25
+    # find monthly savings
+    monthly_savings = annual_salary * percent_salary_saved / 12
+    # calculate months till down payment reached
+    months_to_save = down_payment / monthly_savings
+    # calculate years, months from months
+    years = months_to_save // 12
+    months = months_to_save % 12
+
+    # format message
+    message = "If you save ${0:.2f} per month, it will take {1:.0f} year(s)" + \
+        " and {2:.0f} month(s) to save enough for the down payment!"
+
+    return message.format(monthly_savings, years, months)
+
+print(time_to_buy_house(100000, 50000, 0.25))
